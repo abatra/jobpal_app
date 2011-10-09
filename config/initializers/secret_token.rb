dc98fd838f62a106de613c04d9fb032bc92bce77
Arjun <arjunbatra03@gmail.com>
2011-10-09 12:43:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobpalApp::Application.config.secret_token = 'd8a46428503fad7c4b2262fa393a100c2575b80504bf0ac3b4f58e99d59db166e402939f54ee2a7d3817ce830f157536789a9b282b74e7601a524c410adfa27c'
